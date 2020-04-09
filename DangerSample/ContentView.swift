3caac951ad67cd42e7400c349b9821db8d5e5b9a
ryoya ito <horusu.dqx@gmail.com>
2020-04-09 16:08:11 +0900

    init

---
+++ DangerSample/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DangerSample
+//
+//  Created by 伊藤凌也 on 2020/04/09.
+//  Copyright © 2020 ry-itto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}

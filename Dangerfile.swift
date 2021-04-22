a2741ceb00bf789ac9aad394cbacf38fbccab05b
ryoya ito <110ryoya@gmail.com>
2021-04-22 16:40:22 +0900

    update

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,5 +1,7 @@
 import Danger 
+import DangerSwiftLint // package: https://github.com/ashfurrow/danger-swiftlint.git
 
 let danger = Danger()
+SwiftLint.lint()
 
 danger.message("Validation passed! 🎉")
